c3a0ea48ee514f217a5885b38bd4c5e06c1c339a
abteilung6 <moeller.tilman@gmail.com>
2022-08-07 20:06:23 +0200

    ui: upload csv file

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,18 +1,50 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { EnvironmentConfig } from "./lib/environment";
+import { AppLayout } from "./components/AppLayout/AppLayout";
+import { Button } from "./components/Button/Button";
 import { FileInput } from "./components/FileInput/FileInput";
 
+const environmentConfig = EnvironmentConfig;
+
 export const App: React.FC = ({}) => {
-  const [_selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const uploadFile = () => {
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append("file", selectedFile);
+      axios
+        .post(`${environmentConfig.apiUrl}/transactions/sparkasse`, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then(() => {
+          setSelectedFile(null);
+          console.log("Upload succeed.");
+        })
+        .catch((e) => console.log("Upload failed.", e));
+    }
+  };
 
   const render = (): React.ReactElement => {
     return (
-      <div>
+      <AppLayout>
         <FileInput
-          label="File upload"
+          label={selectedFile ? selectedFile.name : "File upload"}
           accept=".csv"
           onChange={(e) => setSelectedFile(e.target.files![0])}
         />
-      </div>
+        <Button
+          variant="primary"
+          className="w-full my-4"
+          disabled={selectedFile === null}
+          onClick={() => uploadFile()}
+        >
+          Upload file
+        </Button>
+      </AppLayout>
     );
   };
 
---
+++ ui/src/components/AppLayout/AppLayout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export interface AppLayoutProps {
+  children: React.ReactNode;
+}
+
+export const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
+  const render = (): React.ReactElement => {
+    return (
+      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">{children}</div>
+    );
+  };
+
+  return render();
+};
